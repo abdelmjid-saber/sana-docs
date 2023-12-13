0b49f640eeb0b1391629b3bd0fdb9ebee914d984
Abdelmjid Saber <87904671+abdelmjid-saber@users.noreply.github.com>
2023-12-13 12:28:23 +0200

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Sana</span>,
+  project: {
+    link: 'https://github.com/abdelmjid-saber/sana-docs',
+  },
+  docsRepositoryBase: 'https://github.com/abdelmjid-saber/sana-docs',
+  footer: {
+    text: 'Sana Docs',
+  },
+}
+
+export default config
